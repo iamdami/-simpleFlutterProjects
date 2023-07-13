d2ebd6d94342add9837b3675a616d0d967c586c7
Dami <onemoredami@gmail.com>
2023-07-13 20:56:45 +0900

    Create list_add_element.dart

---
+++ basics/variableType/list_add_element.dart
@@ -0,0 +1,17 @@
+void main() {
+  // List
+  List<String> chacha = ["eye", "nose", "ear"];
+  print(chacha);
+  
+  // index
+  print(chacha.length);
+  
+  chacha.add("mouse");
+  print(chacha.length);
+  print(chacha);
+}
+
+// add() 이용해 List 원소 추가 가능
+// 위 예시의 경우 chacha List에 ["eye", "nose", "ear", "mouse"]로 "mouse"가 추가 됨
+
+ 
