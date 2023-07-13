4f0cebd4f0fa3109b6df66e563931a7ec29f5c84
Dami <onemoredami@gmail.com>
2023-07-13 20:54:06 +0900

    Create var_type_is.dart

---
+++ basics/variableType/var_type_is.dart
@@ -0,0 +1,9 @@
+void main() {
+  int num1 = 10;
+  int num2 = 20;
+  print(num1 is int);
+  print(num1 is String);
+  print(num1 is! int);
+  print(num1 is! String);  
+}
+//변수 타입에 대해서는 is, is! 이런 식으로 적용 가능
