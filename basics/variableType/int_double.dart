e0be3eb1ff1f74df4406b580b8832f09834440c8
Dami <onemoredami@gmail.com>
2023-07-13 20:42:32 +0900

    Create int_double.dart

---
+++ basics/variableType/int_double.dart
@@ -0,0 +1,17 @@
+void main() {
+  // 정수
+  int num1 = 2;
+  int num2 = 4;
+  print(num1 + num2);
+  print(num1 - num2);
+  print(num1 / num2);
+  print(num1 * num2);
+  
+  // 실수
+  double num3 = 2.5;
+  double num4 = 0.5;
+  print(num3 + num4);
+  print(num3 - num4);
+  print(num3 / num4);
+  print(num3 * num4);
+}
