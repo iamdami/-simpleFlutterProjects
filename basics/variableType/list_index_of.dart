2652bec22a1b8ee3b64bfbbad9e233383abcc270
Dami <onemoredami@gmail.com>
2023-07-13 20:57:57 +0900

    Create list_index_of.dart

---
+++ basics/variableType/list_index_of.dart
@@ -0,0 +1,13 @@
+void main() {
+  // List
+  List<String> chacha = ["eye", "nose", "ear"];
+  print(chacha);
+  
+  // index
+  print(chacha.indexOf("nose"));
+}
+
+// indexOf() 이용해 index 순서 알 수 있음
+// 위 예시의 경우 nose의 인덱스 순서는 1이기 때문에 1 출력
+
+ 
