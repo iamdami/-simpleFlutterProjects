dc8608736fabf2e0efc25c832bade52ae5338a79
Dami <onemoredami@gmail.com>
2023-07-13 20:41:32 +0900

    Create var.dart

---
+++ basics/variableType/var.dart
@@ -0,0 +1,11 @@
+void main() {
+  var name1 = "dami";
+  var name2 = "chacha";
+  print(name1);
+  
+  name1 = "flutter";
+  print(name1);
+}
+
+//자동으로 오른쪽 값을 통해 문자형 유추
+//변수 내용 수정은 가능하지만 이미 선언된 변수를 다시 선언하는 건 불가능
