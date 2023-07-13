78b6581141355ce20c2065a8fe5b21bc1f69cfbe
Dami <onemoredami@gmail.com>
2023-07-13 20:51:44 +0900

    Create redefining_value.dart

---
+++ basics/variableType/redefining_value.dart
@@ -0,0 +1,19 @@
+void main() {
+  int num1 = 2;
+  print(num1 + 2);
+  print(num1 - 2);
+  print(num1 / 2);
+  print(num1 * 2);
+  print("-"*10);
+  
+  print(num1 % 2);
+  print(num1 % 3);
+  print("-"*10);
+  
+  num1 ++;
+  print(num1);
+  
+  num1 --;
+  print(num1);
+}
+//++, -- 가능
