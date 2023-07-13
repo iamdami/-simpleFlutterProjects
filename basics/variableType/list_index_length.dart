eb8ea4c44d2da39b8a235a69a75ed6cf43fa9266
Dami <onemoredami@gmail.com>
2023-07-13 20:56:08 +0900

    Create list_index_length.dart

---
+++ basics/variableType/list_index_length.dart
@@ -0,0 +1,16 @@
+void main() {
+  // List
+  List<String> chacha = ["eye", "nose", "ear"];
+  List<int> num1 = [1, 2, 3];
+  print(chacha);
+  
+  // index
+  print(chacha[0]);
+  print(chacha.length);
+}
+
+// index는 0부터 시작
+// 전체 List 길이를 넘어서는 index는 출력 불가
+// List 길이 알고 싶다면 List 변수 이름.length 하면 위 chacha List 경우에는 원소가 3개 있으므로 3이 출력됨
+
+ 
