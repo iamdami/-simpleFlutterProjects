a396d04624a7e0a56ee686679c9a86de07984d91
Dami <onemoredami@gmail.com>
2023-07-13 20:54:34 +0900

    Create and_or.dart

---
+++ basics/variableType/and_or.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool res1 = 12 > 10 && 1 > 0;
+  print(res1);
+  bool res2 = 12 > 10 && 1 < 0;
+  print(res2);
+  bool res3 = 12 > 10 || 1 > 0;
+  print(res3);
+  bool res4 = 12 > 10 || 1 < 0;
+  print(res4);
+}
+
+// &&: and
+// ||: or
