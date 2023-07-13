542cceac13f277b5f205aec1a57c3d043e719519
Dami <onemoredami@gmail.com>
2023-07-13 20:55:19 +0900

    Create list.dart

---
+++ basics/variableType/list.dart
@@ -0,0 +1,13 @@
+void main() {
+  // List
+  List<String> chacha = ["eye", "nose", "ear"];
+  List<int> num1 = [1, 2, 3];
+  print(chacha);
+  print(num1);
+}
+
+// <> 안에 형 지정해주고 변수 선언해주면 됨
+// 이미 변수 형 지정되면 리스트 원소를 다른 형으로 지정 불가
+// ex) List<int> num1 = [1, 2, 3]; 일 때, num1 = [1, 2, "mouse"]; 불가
+
+ 
