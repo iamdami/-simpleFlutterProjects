5b8da716dbd2553e8c08860951efb2e868d87e28
Dami <onemoredami@gmail.com>
2023-07-16 19:29:23 +0900

    Create for1.dart

---
+++ basics/for_while/for1.dart
@@ -0,0 +1,14 @@
+void main() {
+//   int total = 0;
+  List<int> nums = [1, 2, 3, 4, 5];
+//   for (int i = 0; i < nums.length; i++){
+//    total += nums[i];
+//   }
+//   print(total);
+  
+  int total = 0;
+  for (int number in nums){
+    total += number;
+  }
+  print(total);
+}
