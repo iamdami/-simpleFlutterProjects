5ed16fee96c721fa97f7f19fabfc78086e72bc07
Dami <onemoredami@gmail.com>
2023-07-16 19:28:16 +0900

    Create if1.dart

---
+++ basics/if/if1.dart
@@ -0,0 +1,11 @@
+void main() {
+  // if
+  int num1 = 10;
+  if (num1 % 3 == 0){
+    print("나머지가 0 입니다.");
+  }else if(num1 % 3 == 1){
+    print("나머지가 1 입니다.");
+  }else{
+    print("나머지가 2 입니다,");
+  }
+}
