392cb28223d71f0d159cc3662a29419ee22601d0
Dami <onemoredami@gmail.com>
2023-07-16 19:35:08 +0900

    Create arrowFuntion.dart

---
+++ basics/function/arrowFuntion.dart
@@ -0,0 +1,13 @@
+void main() {
+  int res1 = addNums(num1: 10, num2: 20);
+  int res2 = addNums(num1: 20, num2: 30, num3: 40);
+  print("result1: $res1");
+  print("result2: $res2");
+  print("sum: ${res1 + res2}");
+}
+
+addNums({
+  required int num1,
+  required int num2,
+  int num3 = 30,
+}) => num1 + num2 + num3;
