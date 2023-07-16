13653644f4335ba16ae17fcc6e24bae3498e3bb1
Dami <onemoredami@gmail.com>
2023-07-16 19:28:55 +0900

    Create switch1.dart

---
+++ basics/switch/switch1.dart
@@ -0,0 +1,15 @@
+void main() {
+  // switch
+  int num1 = 10;
+  switch (num1 % 3){
+    case 0:
+      print("나머지가 0 입니다.");
+      break;
+    case 1:
+      print("나머지가 1 입니다.");
+      break;
+    default:
+      print("나머지가 2 입니다.");
+      break;
+  }
+}
