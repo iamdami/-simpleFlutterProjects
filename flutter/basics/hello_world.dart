6d6ad7e9cb80cbc9c249061a77b479c36a92dd3f
Dami <onemoredami@gmail.com>
2023-07-17 16:19:55 +0900

    Create hello_world.dart

---
+++ flutter/basics/hello_world.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Hello world!"),
+      ),
+    );
+  }
+}
