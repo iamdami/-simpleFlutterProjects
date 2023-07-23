1852a013b30ca59d841814cf18369e4751ed5c74
Dami <onemoredami@gmail.com>
2023-07-23 22:16:03 +0900

    Create main.dart

---
+++ flutter/randomNumberGenerator/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:rand_num_generator/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ),
+  );
+}
