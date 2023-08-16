a1a73fde75307ef5b2d44a493b649c7a0a0266a4
Dami <onemoredami@gmail.com>
2023-08-16 17:55:24 +0900

    Update home_screen.dart

--- flutter/calendar/lib/screen/home_screen.dart
+++ flutter/calendar/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:flutter/material.dart';
 
@@ -20,19 +21,33 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          Calendar(
-            selectedDay: selectedDay,
-            focusedDay: focusedDay,
-            onDaySelected: onDaySelected,
-          ),
-          SizedBox(height: 10.0),
-          TodayBanner(selectedDay: selectedDay, scheduleCount: 3),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Calendar(
+              selectedDay: selectedDay,
+              focusedDay: focusedDay,
+              onDaySelected: onDaySelected,
+            ),
+            const SizedBox(height: 16.0),
+            TodayBanner(
+              selectedDay: selectedDay,
+              scheduleCount: 3,
+            ),
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ScheduleCard(
+                startTime: 12,
+                endTime: 14,
+                content: "이준혁 바보",
+                color: Colors.indigo,
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 
   onDaySelected(DateTime selectedDay, DateTime focusedDay) {
