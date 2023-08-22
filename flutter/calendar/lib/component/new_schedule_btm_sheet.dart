230ecbf174f875e071be8414f0603bd8f37249c0
Dami <onemoredami@gmail.com>
2023-08-22 21:05:09 +0900

    Update new_schedule_btm_sheet.dart

--- flutter/calendar/lib/component/new_schedule_btm_sheet.dart
+++ flutter/calendar/lib/component/new_schedule_btm_sheet.dart
@@ -1,10 +1,16 @@
 import 'package:calendar_scheduler/const/colors.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:calendar_scheduler/model/category_color.dart';
+import 'package:drift/drift.dart' show Value;
 import 'package:flutter/material.dart';
-
+import 'package:get_it/get_it.dart';
 import 'custom_textField.dart';
 
 class ScheduleBtmSheet extends StatefulWidget {
-  const ScheduleBtmSheet({super.key});
+  final DateTime selectedDate;
+
+  const ScheduleBtmSheet({required this.selectedDate, Key? key})
+      : super(key: key);
 
   @override
   State<ScheduleBtmSheet> createState() => _ScheduleBtmSheetState();
@@ -16,6 +22,7 @@ class _ScheduleBtmSheetState extends State<ScheduleBtmSheet> {
   int? startTime;
   int? endTime;
   String? content;
+  int? selectedColorId;
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +44,8 @@ class _ScheduleBtmSheetState extends State<ScheduleBtmSheet> {
             padding: EdgeInsets.only(bottom: btmInset),
             child: Padding(
               padding: EdgeInsets.only(left: 12.0, right: 12.0, top: 16.0),
-              child: Form( //form 아래의 모든 textFormField들을 컨트롤
+              child: Form(
+                //form 아래의 모든 textFormField들을 컨트롤
                 key: formKey, //form의 컨트롤러처럼 기능
                 autovalidateMode: AutovalidateMode.always,
                 child: Column(
@@ -58,7 +66,26 @@ class _ScheduleBtmSheetState extends State<ScheduleBtmSheet> {
                       },
                     ),
                     SizedBox(height: 20.0),
-                    _ColorPicker(),
+                    FutureBuilder<List<CategoryColor>>(
+                        future: GetIt.I<LocalDatabase>().getCategoryColors(),
+                        builder: (context, snapshot) {
+                          // 데이터가 있고 한번도 값이 세팅된 적이 없고
+                          // 최소한 하나의 값이 snapshot.data에 들어있다면
+                          if (snapshot.hasData &&
+                              selectedColorId == null &&
+                              snapshot.data!.isNotEmpty) {
+                            selectedColorId = snapshot.data![0].id; //첫번째 색이 디폴트
+                          }
+                          return _ColorPicker(
+                            colors: snapshot.hasData ? snapshot.data! : [],
+                            selectedColorId: selectedColorId,
+                            colorIdSetter: (int id) {
+                              setState(() {
+                                selectedColorId = id;
+                              });
+                            },
+                          );
+                        }),
                     SizedBox(height: 20.0),
                     _SaveButton(
                       onPressed: onSavePressed,
@@ -74,19 +101,28 @@ class _ScheduleBtmSheetState extends State<ScheduleBtmSheet> {
     );
   }
 
-  void onSavePressed() {
+  void onSavePressed() async{
     // formKey는 생성했는데 Form 위젯과 결합하지 않았을 때
     if (formKey.currentState == null) {
       return;
     }
     // 에러 없으면
     if (formKey.currentState!.validate()) {
-      print("no error");
       formKey.currentState!.save();
-      print("----------");
-      print(startTime);
-      print(endTime);
-      print(content);
+
+      //스케줄 생성
+      final key = await GetIt.I<LocalDatabase>().createSchedule(
+        SchedulesCompanion(
+          date: Value(widget.selectedDate),
+          startTime: Value(startTime!),
+          endTime: Value(endTime!),
+          content: Value(content!),
+          colorID: Value(selectedColorId!),
+        ),
+      );
+
+      Navigator.of(context).pop();
+      // 일정 작성 후 btm sheet 닫기
     } else {
       print("Error");
     }
@@ -112,7 +148,7 @@ class _Time extends StatelessWidget {
             onSaved: onStartSaved,
           ),
         ),
-        SizedBox(
+        const SizedBox(
           width: 18.0,
         ),
         Expanded(
@@ -144,8 +180,20 @@ class _Content extends StatelessWidget {
   }
 }
 
+typedef ColorIdSetter = Function(int id);
+// ColorIdSetter는 함수인데, 파라미터로 int id 하나 받음
+
 class _ColorPicker extends StatelessWidget {
-  const _ColorPicker({super.key});
+  final List<CategoryColor> colors;
+  final int? selectedColorId;
+  final ColorIdSetter colorIdSetter;
+
+  const _ColorPicker(
+      {required this.colors,
+      required this.selectedColorId,
+      required this.colorIdSetter,
+      Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -153,20 +201,36 @@ class _ColorPicker extends StatelessWidget {
       spacing: 10.0,
       runSpacing: 10.0,
       runAlignment: WrapAlignment.start,
-      children: [
-        renderColor(Colors.pinkAccent.shade100),
-        renderColor(Colors.amber.shade200),
-        renderColor(Colors.teal.shade200),
-        renderColor(Colors.blue.shade200),
-      ],
+      children: colors
+          .map((e) => GestureDetector(
+                onTap: () {
+                  colorIdSetter(e.id);
+                }, //각 색깔 누를 때마다 실행
+                child: renderColor(
+                  e,
+                  selectedColorId == e.id, //선택이 된 상태로 가정
+                ),
+              ))
+          .toList(),
     );
   }
 
-  Widget renderColor(Color color) {
+  Widget renderColor(CategoryColor color, bool isSelected) {
     return Container(
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: color,
+        color: Color(
+          int.parse(
+            "FF${color.hexCode}",
+            radix: 16, //16진수 변환
+          ),
+        ),
+        border: isSelected //색이 선택되었으면 테두리 표시
+            ? Border.all(
+                color: Colors.indigoAccent.shade100,
+                width: 2.0,
+              )
+            : null,
       ),
       width: 32.0,
       height: 32.0,
@@ -190,7 +254,7 @@ class _SaveButton extends StatelessWidget {
               alignment: Alignment.center,
             ),
             onPressed: onPressed,
-            child: Text(
+            child: const Text(
               "Save",
               style: TextStyle(
                 fontSize: 20.0,
