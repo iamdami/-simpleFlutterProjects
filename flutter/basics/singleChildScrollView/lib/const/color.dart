37829a04ddd10120e7d3d6cec9768c88bef53bce
Dami <onemoredami@gmail.com>
2023-08-24 01:00:34 +0900

    Create color.dart

---
+++ flutter/basics/singleChildScrollView/lib/const/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final rainbowColors = [
+  Colors.redAccent.shade100,
+  Colors.deepOrangeAccent.shade100,
+  Colors.yellowAccent.shade100,
+  Colors.green.shade200,
+  Colors.blueAccent.shade100,
+  Colors.indigoAccent.shade100,
+  Colors.purple.shade200,
+];
