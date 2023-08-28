8e7f1c765b337c7a2f2113563baea01325f44d1b
Dami <onemoredami@gmail.com>
2023-08-28 22:27:49 +0900

    Create main.dart

---
+++ flutter/fineDust/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:fine_dust/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
