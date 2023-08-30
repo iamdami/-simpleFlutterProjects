451decc372f0e205a76be9e8a7fcc46b7842e106
Dami <onemoredami@gmail.com>
2023-08-31 00:17:24 +0900

    Update main_card.dart

--- flutter/fineDust/lib/component/main_card.dart
+++ flutter/fineDust/lib/component/main_card.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
-import '../const/colors.dart';
 
 class MainCard extends StatelessWidget {
+  final Color bgColor;
   final Widget child;
-  const MainCard({required this.child, Key?key}) : super(key:key);
+  const MainCard({
+    required this.bgColor,
+    required this.child,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        margin: EdgeInsets.symmetric(horizontal: 10.0),
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(
-    Radius.circular(16.0),
-    ),
-    ),
-    color: lightColor,
-    child: child,
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(16.0),
+        ),
+      ),
+      color: bgColor,
+      child: child,
     );
   }
 }
