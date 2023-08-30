4348cd9ca689ca029df5fe21c50590efde44ae34
Dami <onemoredami@gmail.com>
2023-08-31 00:18:18 +0900

    Update category_card.dart

--- flutter/fineDust/lib/component/category_card.dart
+++ flutter/fineDust/lib/component/category_card.dart
@@ -1,24 +1,37 @@
 import 'package:fine_dust/component/main_card.dart';
+import 'package:fine_dust/utils/data_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:fine_dust/component/main_stat.dart';
-
-import '../const/colors.dart';
+import '../model/stat_and_status_model.dart';
 import 'card_title.dart';
 
 class CategoryCard extends StatelessWidget {
-  const CategoryCard({super.key});
+  final Color darkColor;
+  final Color lightColor;
+  final String region;
+  final List<StatAndStatusModel> models;
+
+  const CategoryCard({
+    required this.darkColor,
+    required this.lightColor,
+    required this.region,
+    required this.models,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 200.0,
       child: MainCard(
+        bgColor: lightColor,
         child: LayoutBuilder(
           builder: (context, constraint) {
             return Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 CardTitle(
+                  bgColor: darkColor,
                   title: "Statistics by category",
                 ),
                 Expanded(
@@ -26,16 +39,22 @@ class CategoryCard extends StatelessWidget {
                     scrollDirection: Axis.horizontal,
                     physics: PageScrollPhysics(),
                     //조금만 움직여도 다음 페이지스크롤로 넘어감
-                    children: List.generate(
-                      10,
-                      (index) => MainStat(
-                        width: constraint.maxWidth / 4, //스크롤 시 4개씩 보여지게
-                        category: "Fine Dust $index",
-                        imgPath: "asset/img/best.png",
-                        level: "Best",
-                        stat: "0㎍/㎥",
-                      ),
-                    ),
+                    children: models
+                        .map(
+                          (model) => MainStat(
+                            category: DataUtils.getItemCodeString(
+                                itemCode: model.itemCode),
+                            imgPath: model.status.imagePath,
+                            level: model.status.label,
+                            stat: "${model.stat.getLevelFromRegion(
+                              region,
+                            )}${DataUtils.getUnitFromItemCode(
+                              itemCode: model.itemCode,
+                            )}",
+                            width: constraint.maxWidth / 3,
+                          ),
+                        )
+                        .toList(),
                   ),
                 )
               ],
