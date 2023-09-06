627a90a6122a06cd486acf5e555accf41b1bf721
Dami <onemoredami@gmail.com>
2023-09-06 18:15:39 +0900

    Update stat_model.dart

--- flutter/fineDust/lib/model/stat_model.dart
+++ flutter/fineDust/lib/model/stat_model.dart
@@ -1,32 +1,85 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+part "stat_model.g.dart";
+
+@HiveType(typeId: 2)
 enum ItemCode {
+  @HiveField(0)
   PM10,
+  @HiveField(1)
   PM25,
+  @HiveField(2)
   NO2,
+  @HiveField(3)
   O3,
+  @HiveField(4)
   CO,
+  @HiveField(5)
   SO2,
 }
 
+@HiveType(typeId: 1)
 class StatModel {
+  @HiveField(0) //한 번 등록한 숫자는 바꾸면 안됨
   final double seoul;
+  @HiveField(1)
   final double gyeonggi;
+  @HiveField(2)
   final double incheon;
+  @HiveField(3)
   final double daejeon;
+  @HiveField(4)
   final double sejong;
+  @HiveField(5)
   final double chungnam;
+  @HiveField(6)
   final double chungbuk;
+  @HiveField(7)
   final double gyeongnam;
+  @HiveField(8)
   final double gwangju;
+  @HiveField(9)
   final double jeonbuk;
+  @HiveField(10)
   final double gyeongbuk;
+  @HiveField(11)
   final double daegu;
+  @HiveField(12)
   final double ulsan;
+  @HiveField(13)
   final double busan;
+  @HiveField(14)
   final double jeonnam;
+  @HiveField(15)
   final double gangwon;
+  @HiveField(16)
   final double jeju;
+  @HiveField(17)
   final DateTime dataTime;
+  @HiveField(18)
   final ItemCode itemCode;
+
+  StatModel({
+    required this.seoul,
+    required this.gyeonggi,
+    required this.incheon,
+    required this.daejeon,
+    required this.sejong,
+    required this.chungnam,
+    required this.chungbuk,
+    required this.gyeongnam,
+    required this.gwangju,
+    required this.jeonbuk,
+    required this.gyeongbuk,
+    required this.daegu,
+    required this.ulsan,
+    required this.busan,
+    required this.jeonnam,
+    required this.gangwon,
+    required this.jeju,
+    required this.dataTime,
+    required this.itemCode,
+  });
 
   // JSON 형태에서부터 데이터 받아오기
   StatModel.fromJson({required Map<String, dynamic> json})
