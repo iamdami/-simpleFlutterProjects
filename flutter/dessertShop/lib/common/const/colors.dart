9a8a6efcd73d946275a20395738d8b2145a9e255
Dami <onemoredami@gmail.com>
2023-09-19 21:29:10 +0900

    Create colors.dart

---
+++ flutter/dessertShop/lib/common/const/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primary_color = Color(0xd8e88bc6);
+const body_text_color = Color(0xff868686);
+const input_bg_color = Color(0xfff8f8f8);
+const input_border_color = Color(0xfffff5ea);
