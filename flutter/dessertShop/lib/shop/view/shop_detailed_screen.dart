fdfc7d62375b49f8badf3ddece6dae3a50a25cbd
Dami <onemoredami@gmail.com>
2023-09-29 00:44:34 +0900

    Update shop_detailed_screen.dart

--- flutter/dessertShop/lib/shop/view/shop_detailed_screen.dart
+++ flutter/dessertShop/lib/shop/view/shop_detailed_screen.dart
@@ -1,4 +1,5 @@
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/product/component/product_card.dart';
 import 'package:actual/shop/component/shop_card.dart';
 import 'package:flutter/material.dart';
 
@@ -9,20 +10,64 @@ class ShopDetailedScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: "iceCream",
-      child: Column(
-        children: [
-          ShopCard(
-            image: Image.asset("asset/imgs/food/iceCream.png"),
-            name: "iceCream",
-            tags: ["iceCream", "sweets", "fruit"],
-            ratings: 4.54,
-            ratingsCount: 100,
-            deliveryTime: 20,
-            deliveryFee: 2500,
-            isDetailed: true,
-            detail: "yum!",
-          )
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          "Menu",
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: ShopCard(
+        image: Image.asset(
+          "asset/imgs/food/iceCream.png",
+          width: 200,
+          height: 300,
+        ),
+        name: "iceCream",
+        tags: ["iceCream", "sweets", "fruit"],
+        ratings: 4.54,
+        ratingsCount: 100,
+        deliveryTime: 20,
+        deliveryFee: 2500,
+        isDetailed: true,
+        detail: "yum!",
       ),
     );
   }
