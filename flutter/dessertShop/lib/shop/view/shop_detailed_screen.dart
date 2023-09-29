d030e853e8609f1b74be3104a09f341d3180352f
Dami <onemoredami@gmail.com>
2023-09-30 02:19:39 +0900

    Update shop_detailed_screen.dart

--- flutter/dessertShop/lib/shop/view/shop_detailed_screen.dart
+++ flutter/dessertShop/lib/shop/view/shop_detailed_screen.dart
@@ -1,21 +1,54 @@
+import 'package:actual/common/const/data.dart';
 import 'package:actual/common/layout/default_layout.dart';
 import 'package:actual/product/component/product_card.dart';
 import 'package:actual/shop/component/shop_card.dart';
+import 'package:actual/shop/model/shop_detailed_model.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class ShopDetailedScreen extends StatelessWidget {
-  const ShopDetailedScreen({super.key});
+  final String id;
+
+  const ShopDetailedScreen({
+    required this.id,
+    Key? key,
+  }) : super(key: key);
+
+  Future<Map<String, dynamic>> getShopDetailed() async {
+    final dio = Dio();
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get(
+      "http://$ip/restaurant/$id",
+      options: Options(headers: {"authorization": "Bearer $accessToken"}),
+    );
+    return resp.data;
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: "iceCream",
-      child: CustomScrollView(
-        slivers: [
-          renderTop(),
-          renderLabel(),
-          renderProducts(),
-        ],
+      child: FutureBuilder<Map<String, dynamic>>(
+        future: getShopDetailed(),
+        builder: (_, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          print(snapshot.data);
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final item = ShopDetailedModel.fromJson(json: snapshot.data!);
+
+          return CustomScrollView(
+            slivers: [
+              renderTop(model: item),
+              renderLabel(),
+              renderProducts(),
+            ],
+          );
+        },
       ),
     );
   }
@@ -52,22 +85,13 @@ class ShopDetailedScreen extends StatelessWidget {
     );
   }
 
-  SliverToBoxAdapter renderTop() {
+  SliverToBoxAdapter renderTop({
+    required ShopDetailedModel model,
+  }) {
     return SliverToBoxAdapter(
-      child: ShopCard(
-        image: Image.asset(
-          "asset/imgs/food/iceCream.png",
-          width: 200,
-          height: 300,
-        ),
-        name: "iceCream",
-        tags: ["iceCream", "sweets", "fruit"],
-        ratings: 4.54,
-        ratingsCount: 100,
-        deliveryTime: 20,
-        deliveryFee: 2500,
+      child: ShopCard.fromModel(
+        model: model,
         isDetailed: true,
-        detail: "yum!",
       ),
     );
   }
