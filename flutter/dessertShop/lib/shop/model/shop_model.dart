325da31af128659bed0b73757f2502ae42365940
Dami <onemoredami@gmail.com>
2023-10-05 23:10:00 +0900

    Update shop_model.dart

--- flutter/dessertShop/lib/shop/model/shop_model.dart
+++ flutter/dessertShop/lib/shop/model/shop_model.dart
@@ -1,4 +1,7 @@
 import 'package:actual/common/const/data.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'shop_model.g.dart';
 
 enum ShopPriceRange {
   expensive,
@@ -6,9 +9,13 @@ enum ShopPriceRange {
   cheap,
 }
 
+@JsonSerializable()
 class ShopModel {
   final String id;
   final String name;
+  @JsonKey(
+    fromJson: pathToUrl,
+  )
   final String thumbUrl;
   final List<String> tags;
   final ShopPriceRange priceRange;
@@ -29,21 +36,30 @@ class ShopModel {
     required this.deliveryFee,
   });
 
-  factory ShopModel.fromJson({
-    required Map<String, dynamic> json,
-  }) {
-    return ShopModel(
-      id: json["id"],
-      name: json["name"],
-      thumbUrl: "http://$ip${json["thumbUrl"]}",
-      tags: List<String>.from(json["tags"]),
-      priceRange: ShopPriceRange.values.firstWhere(
-            (e) => e.name == json["priceRange"],
-      ),
-      ratings: json["ratings"],
-      ratingsCount: json["ratingsCount"],
-      deliveryTime: json["deliveryTime"],
-      deliveryFee: json["deliveryFee"],
-    );
+  factory ShopModel.fromJson(Map<String, dynamic> json)
+  => _$ShopModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ShopModelToJson(this);
+
+
+  static pathToUrl(String value){
+    return "http://$ip$value";
   }
+  // factory ShopModel.fromJson({
+  //   required Map<String, dynamic> json,
+  // }) {
+  //   return ShopModel(
+  //     id: json["id"],
+  //     name: json["name"],
+  //     thumbUrl: "http://$ip${json["thumbUrl"]}",
+  //     tags: List<String>.from(json["tags"]),
+  //     priceRange: ShopPriceRange.values.firstWhere(
+  //           (e) => e.name == json["priceRange"],
+  //     ),
+  //     ratings: json["ratings"],
+  //     ratingsCount: json["ratingsCount"],
+  //     deliveryTime: json["deliveryTime"],
+  //     deliveryFee: json["deliveryFee"],
+  //   );
+  // }
 }
