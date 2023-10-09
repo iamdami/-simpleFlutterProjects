d0b3f8586f2b694b0e008462f1366dd92313635e
Dami <onemoredami@gmail.com>
2023-10-10 02:12:59 +0900

    Create shop_detailed_model.g.dart

---
+++ flutter/dessertShop/lib/shop/model/shop_detailed_model.g.dart
@@ -0,0 +1,63 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shop_detailed_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ShopDetailedModel _$ShopDetailedModelFromJson(Map<String, dynamic> json) =>
+    ShopDetailedModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      thumbUrl: DataUtils.pathToUrl(json['thumbUrl'] as String),
+      tags: (json['tags'] as List<dynamic>).map((e) => e as String).toList(),
+      priceRange: $enumDecode(_$ShopPriceRangeEnumMap, json['priceRange']),
+      ratings: (json['ratings'] as num).toDouble(),
+      ratingsCount: json['ratingsCount'] as int,
+      deliveryTime: json['deliveryTime'] as int,
+      deliveryFee: json['deliveryFee'] as int,
+      detail: json['detail'] as String,
+      products: (json['products'] as List<dynamic>)
+          .map((e) => ShopProductModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$ShopDetailedModelToJson(ShopDetailedModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'thumbUrl': instance.thumbUrl,
+      'tags': instance.tags,
+      'priceRange': _$ShopPriceRangeEnumMap[instance.priceRange]!,
+      'ratings': instance.ratings,
+      'ratingsCount': instance.ratingsCount,
+      'deliveryTime': instance.deliveryTime,
+      'deliveryFee': instance.deliveryFee,
+      'detail': instance.detail,
+      'products': instance.products,
+    };
+
+const _$ShopPriceRangeEnumMap = {
+  ShopPriceRange.expensive: 'expensive',
+  ShopPriceRange.medium: 'medium',
+  ShopPriceRange.cheap: 'cheap',
+};
+
+ShopProductModel _$ShopProductModelFromJson(Map<String, dynamic> json) =>
+    ShopProductModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      imgUrl: DataUtils.pathToUrl(json['imgUrl'] as String),
+      detail: json['detail'] as String,
+      price: json['price'] as int,
+    );
+
+Map<String, dynamic> _$ShopProductModelToJson(ShopProductModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'imgUrl': instance.imgUrl,
+      'detail': instance.detail,
+      'price': instance.price,
+    };
